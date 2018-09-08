db01dd853b770e6568d97acb44f957dd36504767
Nithin Chalapathi <nithin.ch10@gmail.com>
2018-09-08 17:56:13 -0600

    Kindle debugging

--- changeColor.js
+++ changeColor.js
@@ -1,1 +1,4 @@
-document.body.style.backgroundColor = "#8B008B";
+//var x = document.getElementById("a:4");
+//x.style.backgroundColor = "#8B008B";
+console.log(document.getElementById("KindleReaderContainer"));
+//https://stackoverflow.com/questions/10066100/google-chrome-extension-manipulate-dom-of-open-or-current-tab
