7e84e6074a412ed31d425e39fc49a60973724b76
Aidan Irish <aidanpirish@gmail.com>
2018-09-09 00:58:40 -0600

    overlay is working on kindle reader

--- changeColor.js
+++ changeColor.js
@@ -1,4 +1,13 @@
 //var x = document.getElementById("a:4");
 //x.style.backgroundColor = "#8B008B";
-console.log(document.getElementById("KindleReaderContainer"));
+
+
+
+
+// content_iframe = document.getElementById('column_0_frame_1');
+// console.log(content_iframe)
+// content_doc = content_iframe.contentDocument || content_iframe.contentWindow.document;
+
+
+// console.log(content_doc.getElementById('a:I5'));
 //https://stackoverflow.com/questions/10066100/google-chrome-extension-manipulate-dom-of-open-or-current-tab
