c0247d759f3f18cc7a982b2ef59e5965a32ad6ed
Nithin Chalapathi <nithin.ch10@gmail.com>
2018-09-09 01:41:12 -0600

    implement removeBars partially

--- popup.js
+++ popup.js
@@ -35,3 +35,14 @@ removeTextColor.onclick = function(element) {
     );
   });
 };
+
+let removeBars = document.getElementById('removeBars');
+
+removeBars.onclick = function(element) {
+  chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {file:"removeBars.js"}
+    );
+  });
+};
---
+++ removeBars.js
@@ -0,0 +1,5 @@
+var cols = [].slice.call(document.getElementsByClassName("column"));
+
+cols.forEach(function(elem) {
+  elem.style.opacity = 0;
+});
