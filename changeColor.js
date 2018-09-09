21f16e977c601dffd9b7882a08d30a8b7e517591
Aidan Irish <aidanpirish@gmail.com>
2018-09-08 23:07:54 -0600

    grid is working

--- changeColor.js
+++ changeColor.js
@@ -29,14 +29,76 @@ divs.forEach(function(element) {
 });
 console.log(total);*/
 
+// set number of columns you want to be displayed
+var colNum = 18;
+
+// colors to display
+color1 = 'red';
+color2 = 'blue';
+
+
+
 nodes = [];
 for (var i = 0; i < divs.length; i++) {
   if (divs[i].length !== 0) {
     var node = divs[i][0].parentNode.insertBefore(document.createElement('div'), divs[i][0]);
 
-    var overlayDiv = document.createElement('div');
-    overlayDiv.classList.add('overlay');
-    node.prepend(overlayDiv);
+    var overlaysDiv = document.createElement('div');
+    overlaysDiv.classList.add('overlays');
+    node.prepend(overlaysDiv);
+    height = 0;
+    divs[i].forEach(function(element) {
+      height = height + element.clientHeight
+    });
+
+    console.log(height)
+
+    //var paragraphHeight = node.clientHeight;
+    
+    nodeChildren = node.children
+
+    console.log(nodeChildren)
+
+    // for (var i = 0; i < nodeChildren.length; i++) {
+    //     console.log(nodeChildren.item(i)); //second console output
+    // }
+
+
+    // add properties to div
+    ;
+
+    overlaysDiv.style.position = 'absolute';
+    overlaysDiv.style.display = 'grid';
+    overlaysDiv.style.gridTemplateColumns = `repeat(${colNum}, 1fr)`
+    overlaysDiv.style.gridTemplateRows = '1fr';
+    overlaysDiv.style.width = '100vw';
+    overlaysDiv.style.height = (height+20) + "px";
+    
+
+    // for each colNum, create col
+    for (var j = 0; j < colNum; j++) {
+      // create a column item
+      var colDiv = document.createElement("div");
+      colDiv.classList.add("column");
+      // alternate all even columns with color
+      if (j % 2 === 0) {
+        if (j % 4 === 0) {
+          colDiv.style.backgroundColor = color1;
+          colDiv.style.opacity = 0.7;
+        } else {
+          colDiv.style.backgroundColor = color2;
+          colDiv.style.opacity = 0.7;
+        }
+      }
+      // assign grid index to div
+      colDiv.style.gridColumn = `${j + 1};`;
+      colDiv.style.gridRow = "1";
+      // append to display div
+      overlaysDiv.appendChild(colDiv);
+    }
+    
+    
+    
 
     divs[i].forEach(function(element) {
       var e = element.parentNode.removeChild(element);
@@ -47,8 +109,21 @@ for (var i = 0; i < divs.length; i++) {
 };
 
 nodes.forEach(function(element){
-  element.style.color = "#ff00ff";
+  console.log(element)
 });
+
+// when user resizes screen, update paragraph height to grid
+// window.onresize = function() {
+//   for (var i = 0; i < paragraphs.length; i++) {
+//     // the overlay is the first child of paragraph div
+//     var overlaysDiv = paragraphs[i].children[0];
+//     // get height of the paragraph
+//     paragraphHeight = paragraphs[i].clientHeight;
+//     // set the div to the exact height of the paragraph
+//     overlaysDiv.style.height = paragraphHeight + "px";
+//   }
+//   location.reload();
+// };
 // for(var i = 0; i < divs.length-1; i ++) {
 //   console.log(i);
 //   var inserted = divs[i][0].insertBefore(document.createElement("div"), null);
